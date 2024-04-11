9e3f9b52c6823ae77f131b2811f7c673c1652e91
LauraMartinsx <melhoralunamartins@gmail.com>
2024-04-11 14:18:56 -0300

    create dialog

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let data
+
+const modalBtn = document.getElementById("close-button")
+const dialog = document.getElementById("dialog")
+const board = document.getElementById("board")
+
+window.addEventListener('load', fetchInformation)
+
+modalBtn.addEventListener('click', closeModal)
 function closeModal() {
-    
+    dialog.style.display = "none"  
 }
+
+
+async function fetchInformation() {
+    await fetch('./data.json')
+    .then((response) => response.json())
+    .then((json) => data = json);
+
+    console.log(data)
+}
+
