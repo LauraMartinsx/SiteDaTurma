be3139ce07e271efe8849d6e9cc5f9a83f76aff7
LauraMartinsx <melhoralunamartins@gmail.com>
2024-04-18 07:19:21 -0300

    Finished Thing

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let data
+let currentCategory
 
 const boardSelectors = {
     courseBtn: document.getElementById("course"),
@@ -6,28 +7,33 @@ const boardSelectors = {
     studentsBtn: document.getElementById("students"),
 }
 
-const modalBtn = document.getElementById("close-button")
+const dialogBtn = document.getElementById("close-button")
 const dialog = document.getElementById("dialog")
 const board = document.getElementById("board")
+
+const titleDialog = document.getElementById("dialog-title")
+const contentDialog = document.getElementById("dialog-content")
+
 let buttonCard = document.querySelectorAll('.button-card')
 
+window.addEventListener('load', onPageLoad)
 
-
-
-window.addEventListener('load', fetchInformation)
-
-modalBtn.addEventListener('click', handleCloseModal)
+dialogBtn.addEventListener('click', handleCloseDialog)
 
 boardSelectors.courseBtn.addEventListener('click', fetchCourseInfo)
 boardSelectors.teacherBtn.addEventListener('click', fetchTeachersInfo)
 boardSelectors.courseBtn.addEventListener('click', fetchCourseInfo)
 
 
+
 handlePressButton()
 
 
 
-
+async function onPageLoad() {
+    await fetchInformation()
+    fetchCourseInfo()
+}
 
 async function fetchInformation() {
     await fetch('./data.json')
@@ -35,51 +41,13 @@ async function fetchInformation() {
     .then((json) => data = json.data);
 }
 
-
-function handleCloseModal() {
-    dialog.style.display = "none"  
-}
-
-function handleOpenModal(button) {
-    console.log(button.target.id)
-    
-}
-
 function handlePressButton() {
     buttonCard.forEach(button =>{ 
-        button.addEventListener('click', (e) => handleOpenModal(e))
+        button.addEventListener('click', (e) => handleOpenDialog(e))
     }) 
 }
 
-function fetchCourseInfo() {
-    const courseInfo = data.courses
 
-    courseInfo.forEach((data) => {
-
-        
-        board.innerHTML = `
-        <div class="card">
-        <img src="${data.imgUrl}" alt="">
-        
-        <div class="card-content">
-            <div>
-                <h2>${data.title}</h2>
-                <span>${data.slug}</span>    
-            </div>
-            <button id="${data.id}" class="button-card w-full">
-                Saiba mais
-            </button>        
-        </div>
-        
-    
-        </div>
-        `
-    })
-
-    buttonCard = document.querySelectorAll('.button-card')
-
-    handlePressButton()
-}
 
 function createCard(info) {
     let stringElement = ``
@@ -104,14 +72,17 @@ function createCard(info) {
         </div>
         `
 
-        console.log(stringElement)
-        
+
         board.innerHTML = stringElement
     })
 }
 
+function getListOfElementsByCategory(category) {
+    return data.filter(item => item.category === category)
+}
+
 function fetchCourseInfo() {
-    const courseInfo = data.courses
+    const courseInfo = getListOfElementsByCategory('course')
 
     createCard(courseInfo)
 
@@ -120,8 +91,19 @@ function fetchCourseInfo() {
     handlePressButton()
 }
 
+function fetchStudentInfo() {
+    const studentInfo = getListOfElementsByCategory('course')
+
+
+    createCard(studentInfo)
+
+    buttonCard = document.querySelectorAll('.button-card')
+
+    handlePressButton()
+}
+
 function fetchTeachersInfo() {
-    const teacherInfo = data.teachers
+    const teacherInfo = getListOfElementsByCategory('teacher')
 
     createCard(teacherInfo)
 
@@ -129,3 +111,26 @@ function fetchTeachersInfo() {
 
     handlePressButton()
 }
+
+function getCardInfoById(id) {
+    const response = data.find(card => card.id === id)
+
+    return response
+}
+
+function handleOpenDialog(info) {
+    const buttonId = info.target.id 
+    const cardInfo = getCardInfoById(buttonId)
+
+
+    titleDialog.textContent = cardInfo.title
+    
+    contentDialog.textContent = cardInfo.content
+
+    dialog.style.display = "flex"  
+}
+
+function handleCloseDialog() {
+    dialog.style.display = "none"  
+}
+
